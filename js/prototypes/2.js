45769e23d2e4a47654d2ad9efcb9b232e6299d51
Natalia21 <natalia1992.92@mail.ru>
2023-12-04 14:23:52 +0300

    added explanation link

--- js/prototypes/2.js
+++ js/prototypes/2.js
@@ -4,3 +4,5 @@ console.log('prop' in obj === obj.hasOwnProperty('prop'));
 console.log('toString' in obj === obj.hasOwnProperty('toString'));
 
 // What will be the output?
+
+// Try yourself and read the explanation: https://t.me/intspirit/884?comment=1267
