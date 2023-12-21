f14274d349a14bf373cb580811c18307c0040347
Natalia21 <natalia1992.92@mail.ru>
2023-12-21 10:48:38 +0300

    added the explanation link

--- js/prototypes/1.js
+++ js/prototypes/1.js
@@ -3,3 +3,5 @@ function MyFunc() {}
 console.log(Object.getPrototypeOf(MyFunc.prototype));
 
 // What will be the output?
+
+// Try yourself and read the explanation: https://t.me/intspirit/902?comment=1305
