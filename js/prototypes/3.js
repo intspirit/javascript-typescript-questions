e97e23ee1a9142d4fa4184c8c5411c9dcc861a04
Natalia21 <natalia1992.92@mail.ru>
2023-12-29 11:43:08 +0300

    added the explanation link

--- js/prototypes/3.js
+++ js/prototypes/3.js
@@ -8,3 +8,5 @@ console.log(Object.hasOwn(myFunc.prototype, 'constructor'));
 console.log(Object.hasOwn(myArrowFunc.prototype, 'constructor'));
 
 // What will be the output?
+
+// Try yourself and read the explanation: https://t.me/intspirit/906?comment=1313
