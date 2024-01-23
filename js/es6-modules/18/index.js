044c0f1c14383853acc701809b663ec3fa536e03
Natalia21 <natalia1992.92@mail.ru>
2024-01-23 10:21:33 +0300

    added the explanation link

--- js/es6-modules/18/index.js
+++ js/es6-modules/18/index.js
@@ -3,3 +3,7 @@ import thing from './module.js';
 setTimeout(() => {
   console.log(thing);
 }, 1000);
+
+// What will be the output?
+
+// Try yourself and read the explanation: https://t.me/intspirit/924?comment=1360
