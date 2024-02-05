5cf516cb592955ccc7582ddff8d1b1efb20dd772
Natalia21 <natalia1992.92@mail.ru>
2024-02-05 14:20:29 +0300

    added the explanation link

--- js/strings/4.js
+++ js/strings/4.js
@@ -2,3 +2,5 @@ console.log(String('Hello') === 'Hello');
 console.log(new String('Hello') === 'Hello');
 
 // What will be the output?
+
+// Try yourself and read the explanation: https://t.me/intspirit/934?comment=1383
