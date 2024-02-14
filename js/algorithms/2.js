0720f22dad1b5c4f1a8e5492673d82ab4fc0c7fb
Natalia21 <natalia1992.92@mail.ru>
2024-02-14 20:17:14 +0300

    updated the quiz

--- js/algorithms/2.js
+++ js/algorithms/2.js
@@ -1,13 +1,17 @@
-function foo(i) {
-  if (i > 10) {
-    return i;
+function foo(n) {
+  const bar = (i) => {
+    if (i > n) {
+      return i;
+    }
+
+    return bar(i + 1) + bar(i + 2);
   }
 
-  return foo(i + 1) + foo(i + 2);
+  return bar(0);
 }
 
-console.log(foo(0));
+console.log(foo(10));
 
-// What will be the output?
+// What is the approximate complexity of the algorithm?
 
 // Try yourself and read the explanation: https://t.me/intspirit/942?comment=1400
