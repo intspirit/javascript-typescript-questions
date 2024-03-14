38d9cdcfd31991db7b9dd0cebefc0bd28c444acf
Natalia21 <natalia1992.92@mail.ru>
2024-03-14 12:14:51 +0300

    added array and algorithms quizes

---
+++ js/algorithms/9.ts
@@ -0,0 +1,10 @@
+// Given an array of unique numbers, you need to find two numbers
+// that add up to the given number.
+function twoSum(nums: number[], target: number): number[] {
+
+};
+
+// Using additional memory to solve the problem
+// best way what data structure will you use?
+
+// Try yourself and read the explanation: https://t.me/intspirit/956?comment=1437
---
+++ js/arrays/12.js
@@ -0,0 +1,5 @@
+const arr = new Array(5).map((i, j) => j);
+
+console.log(arr);
+
+// What will be the output?
