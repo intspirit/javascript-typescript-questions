fdd0ddedbafd4709492b3f3825f78dc8d00d0e63
Natalia21 <natalia1992.92@mail.ru>
2024-03-18 16:35:01 +0300

    added regexp quiz

---
+++ js/other/27.js
@@ -0,0 +1,10 @@
+const reg = /\-/ig;
+
+let match1 = reg.exec('2-1-1');
+let match2 = reg.exec('2-1-2');
+let match3 = reg.exec('2-1-3');
+
+console.log(match1?.index, match2?.index, match3?.index);
+
+// What will be the output?
+
